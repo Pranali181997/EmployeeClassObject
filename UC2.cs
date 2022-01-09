f464fbd475d9bed5f61015c5fb22df00921208c5
Pranali <pranalilambat00@gmail.com>
2022-01-09 23:43:53 +0530

    message

---
+++ UC2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeObjectClass
+{
+    class UC2
+    {
+        public void DailyWages()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+            {
+                if (number == 0)
+                {
+                    int WagePerHr = 20;
+                    int FullDayHr = 8;
+                    int DailyWages = WagePerHr *= FullDayHr;
+                    {
+                        Console.WriteLine("Daily Employee Wages are=" + DailyWages);
+                    }
+                }
+                else
+                {
+                    int DailyWages = 0;
+                    Console.WriteLine("Daily Employee Wages are=" + DailyWages);
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ UC3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeObjectClass
+{
+    class UC3
+
+    {
+        public void PartTime()
+        {
+            Random random = new Random();
+            int number = random.Next(3);
+            {
+                int WagePerHr = 20;
+                int FullTime = 2;
+                int PartTime = 1;
+                //int AbsentTimeHr = 0;
+                //int DailyWages = WagePerHr *= FullTimeHr;
+                if (number == FullTime)
+                {
+                    int FullTimeHr = 8;
+                    int DailyWages = WagePerHr *= FullTimeHr;
+                    Console.WriteLine("Full Time Employee Wages are=" + DailyWages);
+                }
+                else if (number == PartTime)
+                {
+                    int PartTimeHr = 4;
+                    int DailyWages = WagePerHr *= PartTimeHr;
+
+                    Console.WriteLine("Part Time Employee Wages are=" + DailyWages);
+                }
+                else
+                {
+                    int DailyWages = 0;
+                    Console.WriteLine("Employee Is Absent Then Wages are=" + DailyWages);
+                }
+            }
+        }
+    }
+}
+
+    
+    
+
